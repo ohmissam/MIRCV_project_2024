e15d16de314f0480db1f77a05d2c6a0e142874a6
ohmissam <69471565+ohmissam@users.noreply.github.com>
2024-10-16 18:17:10 +0200

    initial cli

---
+++ components/src/main/java/it/unipi/dii/aide/mircv/Config.java
@@ -0,0 +1,23 @@
+package it.unipi.dii.aide.mircv;
+
+import java.util.Arrays;
+
+public class Config {
+
+    //String for the path of stopwords file
+    public final static String STOPWORDS_PATH = "data\\stopwords.txt";
+
+    //String for the document collection path
+    public final static String COLLECTION_PATH = "data\\collection.tsv";
+
+    //String for the path of
+    public static final String LEXICON_FILE_PATH = "data\\Lexicon.txt";
+
+    //String for the path of
+    public static final String DOCINDEX_FILE_PATH = "data\\DocumentIndex.txt";
+
+    //String for the path of
+    public static final String INVINDEX_FILE_PATH = "data\\InvertedIndex.txt";
+
+
+}
